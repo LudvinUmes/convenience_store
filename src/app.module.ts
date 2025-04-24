22002a334b2afe2d212cdbdd51f8b6b816fd4cc4
DaRSZuki <davidnorberto590@gmail.com>
2025-04-24 01:17:07 -0600

    ModuloInventarios1.0

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { ProductosModule } from './productos/productos.module';
 import { VentasModule } from './ventas/ventas.module';
 import { DevolucionesModule } from './devoluciones/devoluciones.module';
+import { InventarioModule } from './inventario/inventario.module';
+import { InventarioModule } from './inventario/inventario.module';
 
 @Module({
-  imports: [ProductosModule, VentasModule, DevolucionesModule],
+  imports: [ProductosModule, VentasModule, DevolucionesModule, InventarioModule],
   controllers: [],
   providers: [],
 })
