9e1628ec77fcafc62ced89bb5d7ed9ae462eda51
DaRSZuki <davidnorberto590@gmail.com>
2025-04-25 19:35:40 -0600

    agregando trigger y funciones

---
+++ db/functions/obtenerPrimerLoteIngresado.sql
@@ -0,0 +1,28 @@
+CREATE OR REPLACE FUNCTION obtenerPrimerLoteIngresado(idProducto INT)
+RETURNS TABLE (
+  id_lote INT,
+  costo_unitario NUMERIC(20,2),
+  precio_lote NUMERIC(10,2),
+  stock INT,
+  fecha_ingreso TIMESTAMP,
+  fecha_vencimiento TIMESTAMP,
+  descripcion TEXT
+) AS $$
+BEGIN
+  RETURN QUERY
+  SELECT
+    lp.id,
+    lp.costo_unitario,
+    lp.precio_lote,
+    lp.stock,
+    lp.fecha_ingreso,
+    lp.fecha_vencimiento,
+    lp.descripcion
+  FROM lote_productos lp
+  WHERE lp.id_producto = idProducto
+    AND lp.estado = 1
+    AND lp.stock > 0
+  ORDER BY lp.fecha_ingreso ASC
+  LIMIT 1;
+END;
+$$ LANGUAGE plpgsql;
