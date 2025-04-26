652951a4770d1fd2812425fe027f9ec73d7c7d29
Alejandro Barrios <josebarriosrodas@gmail.com>
2025-04-25 20:11:45 -0600

    chore: formatting

--- src/categorias/categorias.controller.ts
+++ src/categorias/categorias.controller.ts
@@ -1,4 +1,12 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { CategoriasService } from './categorias.service';
 
 @Controller('categorias')
