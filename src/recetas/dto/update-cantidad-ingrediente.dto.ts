b22956396c318d5cba632ff58c59570fc00a808c
lexisbr <josebarriosrodas@gmail.com>
2025-05-29 05:22:50 -0600

    feat: class validator

--- src/recetas/dto/update-cantidad-ingrediente.dto.ts
+++ src/recetas/dto/update-cantidad-ingrediente.dto.ts
@@ -1,9 +1,13 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsInt, Min } from 'class-validator';
 
 export class UpdateCantidadDto {
   @ApiProperty({ example: 2 })
+  @IsInt()
+  @Min(1, { message: 'La cantidad requerida debe ser al menos 1' })
   cantidad_requerida!: number;
 
   @ApiProperty({ example: 1 })
+  @IsInt()
   usuario_modificacion!: number;
 }
